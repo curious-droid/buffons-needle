f452887ad674a0dccc9fcf65eb63922c392084c5
Curious Droid <64183596+curious-droid@users.noreply.github.com>
2024-09-02 19:41:42 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let totalDrops = 0;
 let hits = 0;
 
 const updatePiEstimate = () => {
-    const piEstimate = (2 * totalDrops) / hits;
+    const piEstimate = (2 * totalDrops) / hits *(1/1); //REPLACE WITH REAL RATIO OF ROLL LENGTH TO LINE SPACING
     document.getElementById('pi-estimate').textContent = isFinite(piEstimate) ? piEstimate.toFixed(5) : "0.00";
     document.getElementById('total-drops').textContent = totalDrops;
     document.getElementById('hits').textContent = hits;
